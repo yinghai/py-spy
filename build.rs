0c7b3d88ec6aeda54cd9f9654f88536675f53c14
Yinghai Lu <yinghai@thinkingmachines.ai>
2025-08-14 22:33:40 +0000

    libunwind

--- build.rs
+++ build.rs
@@ -1,13 +1,27 @@
 use std::env;
 
 fn main() {
-    if env::var("CARGO_CFG_TARGET_ARCH").unwrap() != "x86_64" {
-        return;
-    }
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
-    match env::var("CARGO_CFG_TARGET_OS").unwrap().as_ref() {
-        "windows" => println!("cargo:rustc-cfg=unwind"),
-        "linux" => println!("cargo:rustc-cfg=unwind"),
+    match target_os.as_ref() {
+        "windows" => {
+            if target_arch == "x86_64" {
+                println!("cargo:rustc-cfg=unwind");
+            }
+        },
+        "linux" => {
+            println!("cargo:rustc-cfg=unwind");
+            
+            // Add library search path for aarch64
+            if target_arch == "aarch64" {
+                println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu");
+                // Link to the correct library names for aarch64
+                println!("cargo:rustc-link-lib=unwind");
+                println!("cargo:rustc-link-lib=unwind-ptrace");
+                println!("cargo:rustc-link-lib=unwind-aarch64");
+            }
+        },
         _ => {}
     }
 }
